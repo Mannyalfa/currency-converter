f0a0d884e1a24af2beebbe35e13d57b1d998a7ae
Manny Alfaro <mannyalfa@gmail.com>
2021-10-14 11:58:33 -0400

    fixed dropDowns

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const select = document.querySelectorAll(".currency");
 const btn = document.getElementById("btn");
-const btn = document.getElementById("reset_btn");
+const resetBtn = document.getElementById("reset_btn");
 const num = document.getElementById("num");
 const ans = document.getElementById("ans");
 
@@ -12,14 +12,14 @@ fetch("https://api.frankfurter.app/currencies")
 
 function display(data) {
     const entries = Object.entries(data);
-    for (var i = 0' i < entries.length; i++) {
+    for (var i = 0; i < entries.length; i++) {
         select[0].innerHTML += `<option value ="${entries[i][0]}">${entries[i][0]}</option>`;
     select[1].innerHTML += `<option value ="${entries[i][0]}">${entries[i][0]}</option>`;
 }
 }
 btn.addEventListener("click", () => {
-    let currency1 != select[0].value;
-    let currency2 != select[1].value;
+    let currency1 = select[0].value;
+    let currency2 = select[1].value;
     let value = num.value;
 
     if (currency1 != currency2) {
@@ -39,5 +39,4 @@ function convert(currency1, currency2, value) {
             console.log(Object.values(val.rates)[0]);
             console.log(Object.values(val.rates)[0]);
         });
-
-
+    }
