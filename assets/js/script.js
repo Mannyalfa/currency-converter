b1bb898d23a5e5d30a6ec8459c0cb8ba2f253091
Manny Alfaro <mannyalfa@gmail.com>
2021-10-14 15:42:08 -0400

    refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,33 @@
-const select = document.querySelectorAll(".currency");
-const btn = document.getElementById("btn");
-const resetBtn = document.getElementById("reset_btn");
-const num = document.getElementById("num");
-const ans = document.getElementById("ans");
-
-fetch("https://api.frankfurter.app/currencies")
-    .then((data) => data.json())
+const select = document.querySelectorAll('.currency');
+const number = document.getElementById("number");
+const output = document.getElementById("output");
+fetch('https://api.frankfurter.app/currencies').then((data) => data.json())
     .then((data) => {
         display(data);
     });
-
 function display(data) {
     const entries = Object.entries(data);
     for (var i = 0; i < entries.length; i++) {
-        select[0].innerHTML += `<option value ="${entries[i][0]}">${entries[i][0]}</option>`;
-    select[1].innerHTML += `<option value ="${entries[i][0]}">${entries[i][0]}</option>`;
+        select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]} : ${entries[i][1]}</option>`;
+        select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]} : ${entries[i][1]}</option>`;
+    }
 }
-}
-btn.addEventListener("click", () => {
+function updatevalue() {
     let currency1 = select[0].value;
     let currency2 = select[1].value;
-    let value = num.value;
-
+    let value = number.value;
     if (currency1 != currency2) {
         convert(currency1, currency2, value);
     } else {
-        alert("Choose Different Currency");
+        alert("Choose Diffrent Currency");
     }
-});
-
+}
 function convert(currency1, currency2, value) {
-    const host = "api.franfurter.app";
-    fetch(
-        'https://${host}/latest?amount=${value}&from=$(currency1}&to=${currency2}'
-    )
+    const host = "api.frankfurter.app";
+    fetch(`https://${host}/latest?amount=${value}&from=${currency1}&to=${currency2}`)
         .then((val) => val.json())
         .then((val) => {
             console.log(Object.values(val.rates)[0]);
-            console.log(Object.values(val.rates)[0]);
+            output.value = Object.values(val.rates)[0];
         });
-    }
+}
